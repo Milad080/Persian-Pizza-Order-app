961e5a24d5ce05378609d5cdcef5b285af20db42
Milad <milad.soleymani1380@gmail.com>
2025-10-21 00:04:47 +0330

    fix: switch to HashRouter for SPA deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./UI/Home";
 import Error from "./UI/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
@@ -9,47 +9,40 @@ import CreateOrder, {
 import Order, { loader as orderLoader } from "./features/order/Order";
 import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import AppLayout from "./UI/AppLayout";
-
-const router = createBrowserRouter(
-  [
-    {
-      element: <AppLayout />,
-      errorElement: <Error />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/menu",
-          element: <Menu />,
-          loader: menuLoader,
-          errorElement: <Error />,
-        },
-        {
-          path: "/cart",
-          element: <Cart />,
-        },
-        {
-          path: "/order/:orderId",
-          element: <Order />,
-          loader: orderLoader,
-          errorElement: <Error />,
-          action: updateOrderAction,
-        },
-        {
-          path: "/order/new",
-          element: <CreateOrder />,
-          action: createOrderAction,
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/Persian-Pizza-Order-app/",
-  }
-);
-
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+    ],
+  },
+]);
 function App() {
   return <RouterProvider router={router} />;
 }
