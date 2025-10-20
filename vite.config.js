6498252017281044642a8ef5ac2a67630a3fa212
Milad <milad.soleymani1380@gmail.com>
2025-10-20 20:03:30 +0330

    gzip

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import eslint from "vite-plugin-eslint";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    minify: "esbuild",
+    target: "esnext",
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          state: ["@reduxjs/toolkit", "react-redux"],
+          router: ["react-router-dom"],
+        },
+      },
+    },
+  },
+
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
 });
